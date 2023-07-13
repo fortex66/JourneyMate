e6c50c5ca946da80a023d02a9ca005aab27420e3
SCM <hys1693359@gmail.com>
2023-07-13 15:48:34 +0900

    수정

--- 23_HF124-Backend/routes/sequelize.js
+++ 23_HF124-Backend/routes/sequelize.js
@@ -1,5 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mysql://root:a1234567!@hanium.cgpi8jbee1gd.ap-northeast-2.rds.amazonaws.com:3306/hanium');
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql',
+  });
 
 module.exports = sequelize;
