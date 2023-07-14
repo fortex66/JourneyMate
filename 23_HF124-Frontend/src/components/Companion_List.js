749b07607884d5f0beaa6fcf3e08f20ea5c999e9
SCM <hys1693359@gmail.com>
2023-07-14 18:32:12 +0900

    동행인 수정

--- 23_HF124-Frontend/src/components/Companion_List.js
+++ 23_HF124-Frontend/src/components/Companion_List.js
@@ -1,27 +1,28 @@
-import Navigationbar from "../components/Navigationbar";
-import Companion_List from "../components/Companion_List";
+import React, { useContext } from "react";
+import Companion_Item from "./Companion_Item";
+import { CompanionStateContext } from "../App";
 import styled from "styled-components";
 
-const Companion = () => {
+const Companion_List = () => {
+  const companion_List = useContext(CompanionStateContext);
   return (
-    <div>
-      <CompanionBox>
-        <h1>Companion</h1>
-      </CompanionBox>
-      <Content>
-        <Companion_List />
-      </Content>
-      <Navigationbar />
+    <div className="Companion_List">
+      <List>
+        {companion_List.map((it) => (
+          <Companion_Item key={it.id} {...it}></Companion_Item>
+        ))}
+      </List>
     </div>
   );
 };
-const CompanionBox = styled.div`
-  margin-right: 20px;
-  margin-left: 20px;
-`;
 
-const Content = styled.div`
-  margin-right: 20px;
-  margin-left: 20px;
+Companion_List.defaultProps = {
+  companion_List: [],
+};
+
+const List = styled.div`
+  display: flex; /* Flexbox 사용 */
+  flex-wrap: wrap; /* 창 크기에 따라 자동으로 다음 행으로 넘어가게 설정 */
+  justify-content: space-between; /* 각 아이템 사이에 공간 배분 */
 `;
-export default Companion;
+export default Companion_List;
