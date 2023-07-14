c7bf5e05db8a3829075cfabf92d45bfaabf4d879
구태호 <gth0908@naver.com>
2023-07-14 16:17:00 +0900

    동행인 게시글 상세보기

--- 23_HF124-Frontend/src/pages/Companion.js
+++ 23_HF124-Frontend/src/pages/Companion.js
@@ -1,14 +1,27 @@
 import Navigationbar from "../components/Navigationbar";
 import Companion_List from "../components/Companion_List";
+import styled from "styled-components";
 
 const Companion = () => {
   return (
     <div>
-      <h1>동행인</h1>
-      <Companion_List />
+      <CompanionBox>
+        <h1>Companion</h1>
+      </CompanionBox>
+      <Content>
+        <Companion_List />
+      </Content>
       <Navigationbar />
     </div>
   );
 };
+const CompanionBox = styled.div`
+  margin-right: 20px;
+  margin-left: 20px;
+`;
 
+const Content = styled.div`
+  margin-right: 20px;
+  margin-left: 20px;
+`;
 export default Companion;
