81eb7cedd2232068e211581329e23404370b4374
SCM <hys1693359@gmail.com>
2023-07-24 00:45:24 +0900

    병합 수정

---
+++ 23_HF124-Backend/routes/communityRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {upload}=require('../config');
+const uploadController = require('../controllers/uploadController');
+const commentController = require('../controllers/commentController');
+const authMiddleware=require('../middleware/authMiddleware');
+const postController = require('../controllers/postController');
+const router = express.Router();
+
+
+// authMiddleware를 통과후 controller로 이동
+router.post('/upload', upload.array('photos[]', 10),authMiddleware, uploadController.uploadpost);
+router.delete('/:tpostid', authMiddleware, uploadController.deletepost);
+router.put('/:tpostID', upload.array('photos[]', 10),authMiddleware, uploadController.updatePost);
+router.get('/',authMiddleware, postController.getlist);
+router.get('/:tpostID', upload.array('photos[]', 10),authMiddleware, postController.getpost);
+// comment 부분
+router.post('/comments/:tpostID', authMiddleware, commentController.addComment);
+router.delete('/comments/:tpostID', authMiddleware, commentController.deleteComment);
+
+// 게시글 작성시 키워드 검색으로 위치를 받아오기
+router.get('/posts/search-keyword', uploadController.searchKeyword);
+router.get('/posts', postController.getlist);
+router.get('/posts/:tpostID', postController.getpost);
+
+
+module.exports = router;
---
+++ 23_HF124-Backend/routes/sequelize.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql',
+  });
+
+module.exports = sequelize;
