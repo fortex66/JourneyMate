e77656c17b0996b7d33b417cc9e7c036c4ee0577
SCM <hys1693359@gmail.com>
2023-07-24 21:03:55 +0900

    좋아요 기능

--- 23_HF124-Backend/routes/likeRoutes.js
+++ 23_HF124-Backend/routes/likeRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const { onLike } = require('../controllers/likeController');
-// const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// router.post("/", /*authMiddleware,*/ LikeController.onlike);
-router.post('/like', onLike);
+router.post('/', authMiddleware, onLike);
 
 
 module.exports = router;
