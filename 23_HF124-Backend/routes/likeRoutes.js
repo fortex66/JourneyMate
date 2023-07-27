748e820334b6cf37394473e2ea83baf7ef86cdda
SCM <hys1693359@gmail.com>
2023-07-27 15:04:00 +0900

    스크랩 기능 반영 및 검색필터 결과 전송API

--- 23_HF124-Backend/routes/likeRoutes.js
+++ 23_HF124-Backend/routes/likeRoutes.js
@@ -4,6 +4,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+//좋아요 기능 처리 라우터
 router.post('/', authMiddleware, onLike);
 router.get('/status', authMiddleware, checkLikeStatus);
 router.get('/likeCount', authMiddleware, getLikeCount);
